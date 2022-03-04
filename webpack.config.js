695d52c36da74db11566024269c2138ab620f2f9
Bruno <bdiaz@enalquiler.com>
2022-03-04 18:02:36 +0100

    Add React

--- webpack.config.js
+++ webpack.config.js
@@ -4,20 +4,32 @@ const PUBLIC_ASSETS_PATH = '/';
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(ROOT_PATH, 'src/index.js'),
-  output: {
-    path: path.resolve(ROOT_PATH, 'dist'),
-    filename: '[name].[contenthash].bundle.js',
-    publicPath: PUBLIC_ASSETS_PATH
-  },
-  devServer: {
-      port: 9000,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      publicPath: PUBLIC_ASSETS_PATH,
-      scriptLoading: "defer",
-    })
-  ],
+    entry: path.resolve(ROOT_PATH, 'src/index.js'),
+    output: {
+        path: path.resolve(ROOT_PATH, 'dist'),
+        filename: '[name].[contenthash].bundle.js',
+        publicPath: PUBLIC_ASSETS_PATH,
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/i,
+                use: ["babel-loader"],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    devServer: {
+        port: 9000,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            publicPath: PUBLIC_ASSETS_PATH,
+            scriptLoading: "defer",
+        })
+    ],
 };
