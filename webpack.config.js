c606d215585fcb391ae8d93b355befac0f324cba
Bruno <bdiaz@enalquiler.com>
2022-03-04 17:28:32 +0100

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const ROOT_PATH = path.join(__dirname, '../');
+const PUBLIC_ASSETS_PATH = '/';
+
+
+module.exports = {
+  entry: path.resolve(ROOT_PATH, 'src/index.js'),
+  output: {
+    path: path.resolve(ROOT_PATH, 'dist'),
+    filename: '[name].[contenthash].bundle.js',
+    publicPath: PUBLIC_ASSETS_PATH
+  },
+  devServer: {
+      port: 9000,
+  },
+};
