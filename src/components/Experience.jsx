58e4ecb789998a1f3c139014dd7249ee4eb34018
Bruno <bdiaz@enalquiler.com>
2022-03-06 22:35:53 +0100

    Add styles structure and experience styles

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,48 +1,52 @@
 import React from 'react';
 import Avatar from '../assets/images/avatar4.png';
+import DoctorLogo from '../assets/images/doctor.png';
 
 export function Experience() {
     return (
-        <>
-            <main>
-                <div className="user">
-                    <img className="user__avatar" src={Avatar} alt="" />
-                    <div>
-                        <p className="user__nick">vebris</p>
-                        <p className="user__info">Barcelona (Ciudad) · 8 sep 2019</p>
+        <main className="experience">
+            <div className="experience__user user">
+                <img className="user__avatar" src={Avatar} alt="" />
+                <div className="user__info">
+                    <p className="user__nick">vebris</p>
+                    <p className="user__detail">Barcelona (Ciudad) · 8 sep 2019</p>
+                </div>
+            </div>
+            <h2 className="experience__title">
+                Muy orgullosa de mi nuevo abdomen
+            </h2>
+            <div className="experience__resum">
+                <span className="experience__resum--color">Vale la pena</span>
+                <span className="experience__resum--bw">Precio aproximado: 8500€</span>
+            </div>
+            <div className="experience__gallery">
+                galería
+            </div>
+            <div className="experience__doctor doctor">
+                <h4 className="doctor__title">Sobre el doctor</h4>
+                <div className="doctor__branding">
+                    <img className="doctor__logo" src={DoctorLogo} alt="Dorsia Clínica Estética" />
+                    <div className="doctor__details">
+                        <h2 className="doctor__name">Dorsia Clínica Estética</h2>
+                        <p className="doctor__location">81 sedes en Madrid, Lleida, Tarragona...</p>
                     </div>
                 </div>
-                <h2>
-                    Estoy muy orgullosa de mi nuevo abdomen
-                </h2>
-                <div>
-                    <span>Vale la pena</span>
-                    <span>Precio aproximado: 8500€</span>
-                </div>
-                <div>
-                    galería
-                </div>
-                <div>
-                    <h4>Sobre el doctor</h4>
-                    <div>
-                        logo
-                        <div>
-                            <h7>Dorsia Clínica Estética</h7>
-                            <p>81 sedes en Madrid, Lleida, Tarragona...</p>
-                        </div>
-                    </div>
-                    <button data-message="Contactar al doctor">Contactar al doctor</button>
-                </div>
-                <div>
-                    Valoración
-                </div>
-                <p>Es un excelente cirujano que se merece con creces la buena fama que tiene, pero para estas cosas no hay que escatimar! El post operatorio ha sido un poco difícil. Aún no me adapto a caminar sin sentir molestia y opresión, pero supongo que es cuestión de tiempo.</p>    
-            </main>
+                <button className="doctor__contact" data-message="Contactar al doctor">Contactar al doctor</button>
+            </div>
+            <div className="experience__rating rating">
+                <span className="rating__title">
+                    Valoración:
+                </span>
+                <span className="rating__stars">
+                    *****
+                </span>
+            </div>
+            <p className="experience__text">Es un excelente cirujano que se merece con creces la buena fama que tiene, pero para estas cosas no hay que escatimar! El post operatorio ha sido un poco difícil. Aún no me adapto a caminar sin sentir molestia y opresión, pero supongo que es cuestión de tiempo.</p>    
 
-            <aside>
-                <h4>Cuéntanos tu experiencia</h4>
-                <form></form>
+            <aside className="form">
+                <h4 className="form__title">Cuéntanos tu experiencia</h4>
+                <form className="form__wrap">FORM</form>
             </aside>
-        </>
+        </main>
     );
 }
